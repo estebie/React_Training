87e23bdfa0a63c20998ede2eddcea95aff2edc77
ceniza.sjs <ceniza.sjs@sprobe.com>
2020-01-14 17:23:04 +0800

    Install react router dom and use it for routing pages.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => (
     <div className={`${size} menu-item`}>
         <div
         className='background-image'
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
         style={{
             backgroundImage: `url(${imageUrl})`
         }}/> 
@@ -16,4 +18,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
