e9570ec4ee4d9c2b8cf4bb40cca4cce19b8c6958
ceniza.sjs <ceniza.sjs@sprobe.com>
2020-01-31 17:14:29 +0800

    Implemented user email and password sign-up.

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component{
             })
           });
         }
-
+ 
         this.setState({currentUser:userAuth});
       });
     }
--- src/pages/sign-up-and-sign-in/sign-up-and-sign-in.component.jsx
+++ src/pages/sign-up-and-sign-in/sign-up-and-sign-in.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import SingIn from '../../components/sign-in/sign-in.component';
 
 import './sign-up-and-sign-in.styles.scss';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAdnSignUpPage = () => (
-    <div className='sign-up-and-sign-in'>
+    <div className='sign-in-and-sign-up'>
         <SingIn/>
+        <SignUp/>
     </div>
 );
 
