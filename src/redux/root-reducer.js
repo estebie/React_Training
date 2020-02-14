ee373db1dd5050280bbc94e032aa8b6b1c827b89
ceniza.sjs <ceniza.sjs@sprobe.com>
2020-02-14 10:04:49 +0800

    Implement persistent redux state.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from "redux";
 import userReducer from './user/user.reducer';
 import { cartReducer } from "./cart/cart.reducer";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from "redux-persist";
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whiteList: ['cart']
+}
+
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
