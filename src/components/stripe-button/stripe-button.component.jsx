3743644365965707531a45e47e70e0ab92a22f1e
ceniza.sjs <ceniza.sjs@sprobe.com>
2020-02-17 11:54:33 +0800

    Implement stripe payment integration.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_aUebfPf4J9RobwuJLUZavgFS';
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Sucessful!');
+    }
+    return (
+        <StripeCheckout 
+        label='Pay Now'
+        name='CRWN Clothing Ltd.'
+        billingAddresss
+        shippingAddress
+        image='https://svgshare.com/i/CUz.svg'
+        amount={priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
