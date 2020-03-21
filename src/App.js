0e471101aa78bacd191bafcce6c5c54ee56bf8cb
unknown <ceniza.sjs@sprobe.com>
2020-03-21 12:21:37 +0800

    Implement react hooks on App.js.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 
@@ -17,33 +17,31 @@ import { toggleCartHidden } from './redux/cart/cart.actions';
 import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 import {checkUserSession} from './redux/user/user.actions';
 
-class App extends React.Component{
+const App = ({checkUserSession, currentUser}) => {
 
-
-    componentDidMount(){
-      const {checkUserSession} = this.props;
-      checkUserSession();
-    }
-    render() {
-      return ( 
-        <div className = "App">
-          <Header/>
-          <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route path='/shop' component={ShopPage} />
-            <Route exact path='/checkout' component={CheckoutPage} />
-            <Route exact 
-              path='/signin' 
-              render={()=> this.props.currentUser ? (
-                  <Redirect to='/' />
-                ) : (
-                  <SignInAdnSignUpPage/>
-                  )
-                } 
-            />
-          </Switch>
-        </div>);
-    }
+    useEffect(()=> {
+      checkUserSession()
+    }, [checkUserSession]);
+    
+    return ( 
+      <div className = "App">
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route exact 
+            path='/signin' 
+            render={()=> currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAdnSignUpPage/>
+                )
+              } 
+          />
+        </Switch>
+      </div>);
+    
 }
 
 const mapStateToProps =  createStructuredSelector({
